2b7d3266f5c86b169fffaa4333c6a9fb8f0ef61f
Kevin Egstorf <kevin.egstorf@gmail.com>
2015-04-23 22:21:16 +0200

    added _form.html.erb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,43 +1,42 @@
 class EventsController < ApplicationController
 
   def index
-	@events = Event.all
+    @events = Event.all
   end
 
   def show
-  	@event = Event.find(params[:id])
+    @event = Event.find(params[:id])
   end
-
+  
   def edit
-  	@event = Event.find(params[:id])
+    @event = Event.find(params[:id])
   end
-
+  
   def update
-  	@event = Event.find(params[:id])
-  	@event.update(event_params)
-  	redirect_to @event
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect_to @event
   end
-
+  
   def new
     @event = Event.new
   end
-
+  
   def create
     @event = Event.new(event_params)
     @event.save
     redirect_to @event
-
   end
 
   def destroy
     @event = Event.find(params[:id])
     @event.destroy
     redirect_to events_url
-
-  private
+  end
+    
+private
 
   def event_params
-      event_params = params.require(:event).
-            permit(:name, :description, :location, :price, :starts)
+    params.require(:event).permit(:name, :description, :location, :price, :starts_at)
   end
 end

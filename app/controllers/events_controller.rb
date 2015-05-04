5387800fe435a82b129836aacec5c7dd88854df3
Kevin Egstorf <kevin.egstorf@gmail.com>
2015-05-04 13:36:17 +0200

    added more validation flash message and started with resource db

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,7 +15,7 @@ class EventsController < ApplicationController
   def update
     @event = Event.find(params[:id])
     if @event.update(event_params)
-    redirect_to @event
+       redirect_to @event, notice: "Event Succesfully Updated!"
   else 
     render :edit
   end
